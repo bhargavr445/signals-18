c92c6196a62401bf091eb9a2c02312c16117f4b1
Bhargav R G <bhargavr445@gmail.com>
2024-11-11 20:32:29 -0500

    inline templates

--- src/app/population/population.component.ts
+++ src/app/population/population.component.ts
@@ -1,24 +1,29 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, OnInit, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
-import { Observable, combineLatest, filter, map } from 'rxjs';
+import { filter, map } from 'rxjs';
 import * as actions from '../../app/app-store/app.actions';
 import * as selectors from '../../app/app-store/app.selector';
 import { TableSkeletonComponent } from '../commons/components/table-skeleton/table-skeleton.component';
 import { TableComponent } from '../commons/components/table/table.component';
-import { AuthService } from '../commons/services/api/auth.service';
 import { Datum } from './interfaces/population-responseI';
 
 @Component({
   selector: 'app-population',
   standalone: true,
-  imports: [AsyncPipe, TableComponent, TableSkeletonComponent],
-  templateUrl: './population.component.html',
-  styleUrl: './population.component.scss'
+  imports: [TableComponent, TableSkeletonComponent],
+  template: `
+  <div class="main">
+    @if(!isLoading()) {
+    <gbr-table [tableHeaders]="tableheaders()" [dataList]="dataList()" />
+    } @else {
+    <table-skeleton />
+    }
+  </div>
+  `
 })
 export class PopulationComponent implements OnInit {
 
-  #authService = inject(AuthService)
   #store = inject(Store);
   tableheaders = signal([
     { label: 'Country Name', key: 'Nation', },
@@ -27,22 +32,22 @@ export class PopulationComponent implements OnInit {
     { label: 'Increase/Descrease in %', key: 'diff' }
   ]);
 
-  payload$ = this.#store.select(selectors.populationDataPayloadSelector);
-  dataList$: Observable<Datum[]> = this.#store.select(selectors.populationDataResponseSelector).pipe(
-    filter((d => !!d)),
-    map((resp) => this.calculateIncreasePercentage(resp.data)),
+  dataList = toSignal<Datum[]>(
+    this.#store.select(selectors.populationDataResponseSelector).pipe(
+      filter((d => !!d)),
+      map((resp) => this.#calculateIncreasePercentage(resp.data))),
+    this.#initialValue(null)
   );
-  isLoading$ = this.#store.select(selectors.populationDataLoadingStatusSelector);
+  isLoading = toSignal(
+    this.#store.select(selectors.populationDataLoadingStatusSelector),
+    { initialValue: true }
+  );
 
   ngOnInit(): void {
     this.#store.dispatch(actions.fetchPopulationDataStartAction({ value: 'United States' }));
-
-    combineLatest([
-      this.#authService.userProfileSub$
-    ]).subscribe(d => console.log(d))
   }
 
-  private calculateIncreasePercentage(data: any[]) {
+  #calculateIncreasePercentage(data: any[]): Datum[] {
     return data.map(d => {
       const previousYear = data.find(dl => parseInt(dl.Year) === parseInt(d.Year) - 1);
 
@@ -52,13 +57,12 @@ export class PopulationComponent implements OnInit {
         percentageDifference = ((diff / previousYear.Population) * 100).toFixed(2);
         percentageDifference = (diff >= 0 ? '+' : '') + percentageDifference + '%';
       }
-
-      return {
-        ...d,
-        diff: percentageDifference
-      };
+      return { ...d, diff: percentageDifference };
     });
   }
 
+  #initialValue<T>(value: T) {
+    return { initialValue: value }
+  }
 
 }
