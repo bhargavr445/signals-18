1191012bb00ab413c329ce55d33e576eedf3bb1f
Bhargav R G <bhargavr445@gmail.com>
2025-08-11 22:06:43 -0400

    tested and overwritten input signals from spec

--- src/app/commons/components/table/table.component.spec.ts
+++ src/app/commons/components/table/table.component.spec.ts
@@ -2,29 +2,44 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { inputBinding, provideZonelessChangeDetection, signal } from '@angular/core';
 
 import { TableComponent } from './table.component';
+import { provideHttpClient } from '@angular/common/http';
 
 fdescribe('TableComponent', () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
+  const dataList = signal([]);
+  const headers = signal([{ label: 'Name' }, { label: 'ID' }]);
 
   beforeEach(async () => {
+    // Reset the signals to their initial state for each test
+    dataList.set([]);
+    headers.set([{ label: 'Name' }, { label: 'ID' }]);
+
     await TestBed.configureTestingModule({
-      providers: [provideZonelessChangeDetection()],
+      providers: [provideZonelessChangeDetection(), provideHttpClient()],
       imports: [TableComponent]
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TableComponent, {
       bindings: [
-        inputBinding('dataList', signal([])),
-        inputBinding('tableHeaders', signal([])),
+        inputBinding('dataList', dataList),
+        inputBinding('tableHeaders', headers),
       ]
     });
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create with initial values', () => {
     expect(component).toBeTruthy();
+    expect(component.headersLength()).toEqual(2);
+  });
+
+  it('should update headersLength when headers are changed', () => {
+    // Update the signal for this specific test
+    headers.set([]);
+    fixture.detectChanges();
+    // checking for the 
+    expect(component.headersLength()).toEqual(0);
   });
 });
